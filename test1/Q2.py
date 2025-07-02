c843d67064adbeffbbc78550dad1c34d226c9c39
Mahesh Sanap <msanap752@gmail.com>
2025-07-02 21:59:24 +0530

    Test 1.

---
+++ test1/Q2.py
@@ -0,0 +1,7 @@
+pv=int(input("Enter principle value :"))
+rate=float(input("Enter rate of intrest :"))
+years=int(input("Enter the yeras :"))
+
+simple_intrest=(pv*rate*years)/100
+
+print("simple_intrest is :",simple_intrest)
---
+++ test1/Q3.py
@@ -0,0 +1,6 @@
+km=float(input("Enter the distance in KM :"))
+
+meter=km*1000
+ce=meter*100
+print("meter :",meter)
+print("centimeter :",ce)

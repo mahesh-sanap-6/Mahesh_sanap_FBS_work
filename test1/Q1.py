ebbcc8ddece23fb8c9d6abfbf43205be28da2733
Mahesh Sanap <msanap752@gmail.com>
2025-08-20 18:12:39 +0530

    Delete test1 directory

--- test1/Q1.py
+++
@@ -1,9 +0,0 @@
-length=float(input("Enter the lengtho :"))
-bredth=float(input("Enter the bredth :"))
-radious=float(input("Enter the radious"))
-
-area=length*bredth
-perimeter=area**2
-
-print("area is :",area)
-print("perimeter is :",perimeter)
--- test1/Q3.py
+++
@@ -1,6 +0,0 @@
-km=float(input("Enter the distance in KM :"))
-
-meter=km*1000
-ce=meter*100
-print("meter :",meter)
-print("centimeter :",ce)
